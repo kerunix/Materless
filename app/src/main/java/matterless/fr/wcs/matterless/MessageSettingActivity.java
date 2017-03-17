59ebb4b70341806ccf59bb1fba90e83220a8d411
kerunix <lafont.pierre.wcs@gmail.com>
2017-03-17 11:29:59 +0100

    spinner for day selection

--- app/src/main/java/matterless/fr/wcs/matterless/MessageSettingActivity.java
+++ app/src/main/java/matterless/fr/wcs/matterless/MessageSettingActivity.java
@@ -2,6 +2,8 @@ package matterless.fr.wcs.matterless;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MessageSettingActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MessageSettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_setting);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerDayOfWeek);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.daysOfWeekArray, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
