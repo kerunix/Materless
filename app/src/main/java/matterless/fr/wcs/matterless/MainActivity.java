8c8d1786a2a77c48d2d69a700adcf5d474bc7f45
[Arnaud Barbaria] <[arnaud.barbaria@gmail.com]>
2017-03-17 11:14:53 +0100

    first activity intents are ok

--- app/src/main/java/matterless/fr/wcs/matterless/MainActivity.java
+++ app/src/main/java/matterless/fr/wcs/matterless/MainActivity.java
@@ -3,26 +3,33 @@ package matterless.fr.wcs.matterless;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonConfigureEvents = (Button) findViewById(R.id.buttonConfigureEvents);
 
-        buttonConfigureEvents.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.buttonConfigureEvents:
                 Intent intent = new Intent(MainActivity.this, MessageListActivity.class);
                 startActivity(intent);
-            }
-        });
-
+                break;
+            case R.id.buttonMyProfile:
+                Intent intent1 = new Intent(MainActivity.this, SettingsActivity.class);
+                break;
+        }
 
     }
 }
